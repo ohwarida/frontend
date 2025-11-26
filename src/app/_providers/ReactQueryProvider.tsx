817519f66633d2f736fb91eada322bb4b56b54c6
Chang-KY <pzxcvp0508@gmail.com>
2025-11-26 14:05:59 +0900

    chore: Setting Next App

---
+++ src/app/_providers/ReactQueryProvider.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import React from 'react'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient } from '@tanstack/react-query'
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 30,
+    },
+  },
+})
+
+export default function ReactQueryProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
---
+++ src/app/_providers/index.ts
@@ -0,0 +1,4 @@
+import ReactQueryProvider from '@/app/_providers/ReactQueryProvider'
+// export { default as ReactQueryProvider } from '@/app/_providers/ReactQueryProvider'
+
+export { ReactQueryProvider }
