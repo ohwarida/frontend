12200657dd478603574ee3fd571fa835e1d9ad2f
Chang-KY <pzxcvp0508@gmail.com>
2025-12-10 20:38:14 +0900

    feat: 버튼 디렉토리 이동

--- src/components/ui/Button.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-//todo 버튼 컴포넌트 디자인 하기 (variants, size, )
-export default async function Button({ children }: { children: React.ReactNode }) {
-  return <button className="border px-3 py-1">{children}</button>
-}
