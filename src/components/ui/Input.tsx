2ae994d51dc63829f5af8aa6e5a473a678ab484b
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-12-17 18:55:28 +0900

    feat: Input 컴포넌트 수정
    - FormInput에서 전달된 className을 input 요소에 적용
    - placeholder 기본값 처리

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -6,15 +6,18 @@ type BaseInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
 }
 
 export default function Input({ icon, ...props }: BaseInputProps) {
+  const { className, placeholder, ...rest } = props
+
   return (
     <div className="relative">
       <input
         className={clsx(
           'h-10 w-full rounded-md bg-gray-200 py-2.5 pr-3 text-xs focus:ring-1 focus:ring-gray-300',
           icon ? 'pl-8.5' : 'pl-3',
+          className,
         )}
-        placeholder="검색어를 입력하세요..."
-        {...props}
+        placeholder={placeholder ?? '입력하세요'}
+        {...rest}
       />
       {icon && (
         <span className="absolute inset-y-0 left-2 flex items-center justify-center">{icon}</span>
