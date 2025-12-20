7a977c629bd12117cda2624fb598fd096a1e03ae
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-12-21 03:28:14 +0900

    feat: HeaderElement 컴포넌트 수정
    - 변경된 디자인 시안 반영으로 검색 바 제거
    - isContentCreatePage 판별 조건 수정

--- src/components/layout/header/HeaderElement.tsx
+++ src/components/layout/header/HeaderElement.tsx
@@ -1,39 +1,39 @@
-import React from 'react'
-import Input from '@/components/ui/Input'
 import Link from 'next/link'
 import Button from '@/components/ui/button/Button'
 import ProfileMenu from '@/components/layout/header/ProfileMenu'
-import { Search, SquarePen } from 'lucide-react'
+import { SquarePen } from 'lucide-react'
+import { locationType } from '@/types/Location.types'
 
-export default function HeaderElement({ location }: { location: locationTypes }) {
+export default function HeaderElement({ location }: { location: locationType }) {
   const isHome = location === 'board'
-  const isContentCreatePage = location === 'content'
+  const isContentCreatePage = location === 'content/create'
 
   return (
-    <div className="flex w-full items-center justify-between gap-4">
-      <div className="w-[400px]">
+    <div className="flex items-center gap-3">
+      {/* 검색 넣을 거면 아래처럼 “필요할 때만” flex로 확장 */}
+      {/* <div className="mr-2 w-[400px]">
         <Input icon={<Search size={16} color="#99A1AF" />} type="search" />
-      </div>
-      <div className="flex items-center gap-3">
-        {isHome && (
-          <>
-            <Link href="/content">
-              <Button icon={<SquarePen size={16} />}>글쓰기</Button>
-            </Link>
-            <ProfileMenu />
-          </>
-        )}
-        {isContentCreatePage && (
-          <>
-            <Link href="/">
-              <Button variant="cancel">취소</Button>
-            </Link>
-            <Button variant="add" type="submit" form="postFormId">
-              발행하기
-            </Button>
-          </>
-        )}
-      </div>
+      </div> */}
+
+      {isHome && (
+        <>
+          <Link href="/content">
+            <Button icon={<SquarePen size={16} />}>글쓰기</Button>
+          </Link>
+          <ProfileMenu />
+        </>
+      )}
+
+      {isContentCreatePage && (
+        <>
+          <Link href="/">
+            <Button variant="cancel">취소</Button>
+          </Link>
+          <Button variant="add" type="submit" form="postFormId">
+            발행하기
+          </Button>
+        </>
+      )}
     </div>
   )
 }
