c1d5b84ebec8359a4a90ae74d92cbd67a4800500
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-12-21 15:19:17 +0900

    feat: HeaderElement 컴포넌트 수정
    - 클라이언트 컴포넌트로 전환
    - 유저 역할별 헤더 UI 분기 처리

--- src/components/layout/header/HeaderElement.tsx
+++ src/components/layout/header/HeaderElement.tsx
@@ -1,13 +1,19 @@
+'use client'
+
 import React from 'react'
 import Input from '@/components/ui/Input'
 import Link from 'next/link'
 import Button from '@/components/ui/button/Button'
 import ProfileMenu from '@/components/layout/header/ProfileMenu'
 import { Search, Shield, SquarePen } from 'lucide-react'
+import { useAuthState } from '@/app/hooks/useAuthState'
 
 export default function HeaderElement({ location }: { location: locationTypes }) {
   const isHome = location === 'board'
   const isContentCreatePage = location === 'content'
+
+  const { user } = useAuthState()
+  const isAdmin = user?.role === 'ADMIN'
 
   return (
     <div className="flex w-full items-center justify-between gap-4">
@@ -15,16 +21,18 @@ export default function HeaderElement({ location }: { location: locationTypes })
         <Input icon={<Search size={16} color="#99A1AF" />} type="search" />
       </div>
       <div className="flex items-center gap-3">
-        <>
-          <Link href="/">
-            <Button variant="cancel">관리자 모드 종료</Button>
-          </Link>
-          <Link href="/admin">
-            <Button variant="warning" icon={<Shield size={16} />}>
-              관리자 모드
-            </Button>
-          </Link>
-        </>
+        {isAdmin && (
+          <>
+            <Link href="/">
+              <Button variant="cancel">관리자 모드 종료</Button>
+            </Link>
+            <Link href="/admin">
+              <Button variant="warning" icon={<Shield size={16} />}>
+                관리자 모드
+              </Button>
+            </Link>
+          </>
+        )}
         {isHome && (
           <>
             <Link href="/content">
