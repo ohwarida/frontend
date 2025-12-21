0784fb115f9d6ba061dde7136eabcbfb7a7ba295
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-12-21 18:19:38 +0900

    fix: Main 컴포넌트 레이아웃 수정

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default async function Main({ children }: { children: React.ReactNode }) {
   return (
-    <main className="min-h-[calc(100vh-80px)] bg-gray-50">
-      <div className="mx-auto flex min-h-[calc(100vh-80px)] max-w-7xl gap-5 px-5">{children}</div>
+    <main className="min-h-screen bg-gray-50">
+      <div className="mx-auto flex min-h-screen max-w-7xl gap-5 px-5">{children}</div>
     </main>
   )
 }
