49be6ea047d772a05c5a02f481d359658fcdd63b
kwonyoung <126282666+Chang-KY@users.noreply.github.com>
2025-12-22 14:42:24 +0900

    feat: 컴포넌트 동기화(async 제거)
    Co-authored-by: Suyeon Kim <kimsuyeon_0916@naver.com>

--- src/components/modal/RightModal.tsx
+++ src/components/modal/RightModal.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ModalShell } from '@/components/modal/shell/ModalShell'
 
-export default async function RightModal({
+export default function RightModal({
   returnTo,
   children,
   backdropClassName,
