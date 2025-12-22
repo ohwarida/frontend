4e5fe58bf70e52d3365a594786b453250ff0285c
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-12-22 16:37:24 +0900

    feat: PostEditPage 수정
    - 게시글 상세 조회 연동으로 기존 폼 작성 데이터 불러오기
    - PostForm 적용

--- src/app/(authenticated)/post/[id]/edit/page.tsx
+++ src/app/(authenticated)/post/[id]/edit/page.tsx
@@ -1,5 +1,26 @@
-export default async function PostEditPage({ params }: { params: Promise<{ id: string }> }) {
-  // const post = await server<{ title: string }>(`/api/v1/posts/${params.id}`, { method: 'GET', cache: 'no-store' })
+import { getPostDetail } from '@/features/(authenticated)/post/[id]/apis/post.api'
+import { updatePostAction } from '@/features/(authenticated)/post/create/actions/postUpdateAction'
+import PostForm from '@/features/(authenticated)/post/create/components/PostForm'
 
-  return <div></div>
+export default async function PostEditPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
+  const postId = Number(id)
+
+  const post = await getPostDetail(postId)
+
+  return (
+    <main className="min-h-screen w-full bg-[#F9FAFB]">
+      <PostForm
+        mode="edit"
+        action={updatePostAction}
+        postId={postId}
+        initialValues={{
+          topic: post.topic,
+          title: post.title,
+          content: post.content,
+        }}
+        formKey={postId}
+      />
+    </main>
+  )
 }
