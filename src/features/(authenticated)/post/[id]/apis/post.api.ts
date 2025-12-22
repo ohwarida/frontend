6b255811e0e48c1721f2ba13f0eaf1b90a445298
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-12-22 10:35:50 +0900

    feat: getPosts API 함수 변경
    - 쿼리 파라미터에 topic 추가(추후 반영 예정)
    - API 경로 변경

--- src/features/(authenticated)/post/[id]/apis/post.api.ts
+++ src/features/(authenticated)/post/[id]/apis/post.api.ts
@@ -7,19 +7,24 @@ import type {
   GetPostsResponse,
   UpdatePostRequest,
 } from '../types/Post.types'
+import { TopicType } from '../../create/types/Topic.types'
 
 type GetPostsParams = {
   cursorId?: number
   size?: number
+  topic?: TopicType
 }
 
+// TODO: getPosts가 topic/cursor/size 등을 쿼리로 받을 수 있게 되면, 전체 조회 후 프론트 필터링을 제거하고 서버에서 topic 필터링된 결과만 받아오도록 변경.
 export async function getPosts(params: GetPostsParams = {}): Promise<GetPostsResponse> {
   const searchParams = new URLSearchParams()
+
+  if (params.topic) searchParams.set('topic', params.topic)
   if (typeof params.cursorId === 'number') searchParams.set('cursorId', String(params.cursorId))
   if (typeof params.size === 'number') searchParams.set('size', String(params.size))
   const query = searchParams.toString()
 
-  const res = await server(`/api/v1/posts${query ? `?${query}` : ''}`, {
+  const res = await server(`/api/v1/posts/summary${query ? `?${query}` : ''}`, {
     method: 'GET',
   })
   if (!res.ok) throw new Error('게시글 전체 조회 실패')
