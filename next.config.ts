3f85900e04de8be7d8647857c431de0b2e639d3a
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-12-27 01:53:41 +0900

    chore: next.config 설정 업데이트
    - serverActions bodySizeLimit 10mb로 설정

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  async headers() {
-    const coop = {
-      key: 'Cross-Origin-Opener-Policy',
-      value: 'same-origin-allow-popups',
-    }
-
-    return [
-      {
-        source: '/signin/:path*',
-        headers: [coop],
-      },
-      {
-        source: '/signup/:path*',
-        headers: [coop],
-      },
-    ]
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
   },
 }
 
