041aaf61f638413416a62c1841302e8482f29623
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-12-27 13:25:32 +0900

    feat: Main 레이아웃 컴포넌트 모바일 대응 적용

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default async function Main({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-gray-50">
-      <div className="mx-auto flex max-w-7xl gap-5 px-5">{children}</div>
+      <div className="mx-auto flex w-full max-w-7xl flex-col gap-5 px-0 lg:flex-row lg:px-5">
+        {children}
+      </div>
     </div>
   )
 }
