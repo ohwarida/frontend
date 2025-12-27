86b984c7fcbd59e8d1ebc65e15a4d844351d7979
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-12-27 13:08:12 +0900

    feat: Aside 컴포넌트 모바일 대응 적용

--- src/components/layout/aside/Aside.tsx
+++ src/components/layout/aside/Aside.tsx
@@ -3,7 +3,7 @@ import Dashboard from './Dashboard'
 
 const Aside = () => {
   return (
-    <aside className="sticky top-[89px] h-full min-w-72">
+    <aside className="hidden lg:sticky lg:top-[89px] lg:block lg:h-full lg:min-w-72">
       <div className="space-y-5">
         <Feed />
         <Dashboard />
