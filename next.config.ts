3549b0c8289d942ca9741808a0a5055c3ce3af8b
Chang-KY <pzxcvp0508@gmail.com>
2026-01-23 09:14:23 +0900

    feat: 이미지 패스 설정 재정의

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
         protocol: 'http',
         hostname: 'localhost',
         port: '8080',
-        pathname: '/**',
+        pathname: '/api/v1/users/profiles/**',
       },
     ],
   },
